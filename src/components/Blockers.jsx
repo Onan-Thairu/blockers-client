6c4fd7b44f8d20bd29a0a8f25d96c4ba4c15027c
OnanThairu <thairuonan@gmail.com>
2022-09-06 15:40:57 +0300

    Style header.

--- src/components/Blockers.jsx
+++ src/components/Blockers.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react"
+
 function Blockers() {
-  return(
-    <p>Blockers list</p>
+
+  // const [ blockers, setBlockers ] = useState()
+
+  // useEffect(() => {
+  //   getBlockers()
+  // }, [])
+
+  // const getBlockers = async () => {
+  //   const api = await fetch(`http://localhost:9292/blockers`)
+  //   const data = await api.json()
+  //   console.log(data)
+  //   setBlockers(data)
+  // }
+
+  return (
+    <>
+      <p>Blockers list</p>
+      
+        {/* {
+          blockers.map((blocker) => {
+            return (
+              <div>
+                <p>{ blocker.description }</p>
+                <p>{ blocker.solution }</p>
+              </div>
+            )
+          })
+        } */}
+      
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,46 @@
 import Blockers from "../components/Blockers"
+import styled from "styled-components"
+import { Link } from "react-router-dom"
 
 function Home() {
   return (
-    <Blockers />
+    <>
+      <div>
+        <Header>
+          <h4>Blockers</h4>
+          <Add to='/'>ADD BLOCKER</Add>
+        </Header>
+        <Blockers />
+      </div>
+    </>
   )
 }
+
+const Header = styled.div`
+  display: flex;
+  gap: 60%;
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin-top: 2rem;
+  text-align: center;
+
+  h4 {
+    font-family: 'Dancing Script', cursive;
+    font-weight: 900;
+    font-size: 1.5em;
+    letter-spacing: 0.3em;
+  }
+
+`
+const Add = styled(Link)`
+  text-decoration: none;
+  background-color: #778472;
+  color: #fff;
+  font-size: 0.7em;
+  font-weight: 600;
+  padding: .8rem 1rem;
+  letter-spacing: .1rem;
+  border-radius: .5rem;
+`
 
 export default Home
