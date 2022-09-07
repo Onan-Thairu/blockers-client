96c1ab4c0f93f8a8e7b9d3fac127393da9bc5665
OnanThairu <thairuonan@gmail.com>
2022-09-07 14:59:55 +0300

    Create form.

--- src/pages/CreateBlocker.jsx
+++ src/pages/CreateBlocker.jsx
@@ -2,10 +2,32 @@ import styled from "styled-components"
 
 function CreateBlocker() {
   return (
-    <div>
+    <Wrapper>
       <h4>CreateBlocker</h4>
-    </div>
+      <Form>
+        <label htmlFor="username">USERNAME</label>
+        <input type="text" id="username" name="username" />
+
+        <label htmlFor="blocker">BLOCKER</label>
+        <textarea name="blocker" id="blocker" cols="30" rows="10"></textarea>
+
+        <label htmlFor="tags">TAG</label>
+        <select id="tags" name="tags">
+          <option value="react">React</option>
+          <option value="ruby">Ruby</option>
+          <option value="javascript">Javascript</option>
+        </select>
+      </Form>
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.div`
+  background-color: blue;
+  text-align: center;
+`
+
+const Form = styled.form`
+  display: block;
+`
 export default CreateBlocker
