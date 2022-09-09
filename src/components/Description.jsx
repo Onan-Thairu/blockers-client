c8d6ec07bf59a09834de053c0ca7cb35891c3222
OnanThairu <thairuonan@gmail.com>
2022-09-09 14:39:14 +0300

    Add delete icon.

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components"
 import Solution from "./Solution"
+import { AiOutlineDelete } from 'react-icons/ai'
 
 function Description({blocker}) {
   return (
     <>
       <Container>
+        <h2><AiOutlineDelete /></h2>
         <p>{blocker.description}</p>
         <Solution solution={ blocker.solution} />
       </Container>
@@ -25,6 +27,11 @@ const Container = styled.div`
     margin: 0;
     padding: 0;
     color: black;
+  }
+
+  h2 {
+    display: flex;
+    float: right;
   }
 
   box-shadow: 3px 4px 5px -3px rgba(107,101,101,0.75);
